64e479fec6b7f191ae535c185aa0e0700226ffee
Joshua Swick <joshuaswickirl@gmail.com>
2019-03-23 11:46:59 -0600

    Add unit tests for put, contains, and items methods

--- test_hash_list.py
+++ test_hash_list.py
@@ -45,3 +45,41 @@ def test_put_3():
     _hashlist.put(item3)
     assert _hashlist.hash_list[1] == 69
 
+# HashList.contains(item)
+def test_contains_1():
+    list_length = 10
+    item1 = 10 
+    _hashlist = HashList(list_length)
+    _hashlist.put(item)
+    assert _hashlist.contains(item) == True
+
+def test_contains_2():
+    list_length = 10
+    item1 = 10
+    item2 = 59
+    item3 = 69
+    _hashlist = HashList(list_length)
+    _hashlist.put(item1)
+    _hashlist.put(item2)
+    _hashlist.put(item3)
+    assert _hashlist.contains(item3) == 69
+
+# HashList.items()
+def test_items_1():
+    list_length = 10
+    item1 = 10
+    item2 = 59
+    item3 = 69
+    _hashlist = HashList(list_length)
+    _hashlist.put(item1)
+    _hashlist.put(item2)
+    _hashlist.put(item3)
+    assert _hashlist.items() == [69,10,59]
+
+def test_items_2():
+    list_length = 10
+    items = [1,2,3,4,5,6,7,8,9]
+    _hashlist = HashList(list_length)
+    for item in items:
+        _hashlist.put(item)
+    assert _hashlist.items() == items
